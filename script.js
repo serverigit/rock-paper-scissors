36e786c751da8777adfb5eccdea714395555551e
serverigit <serverigit@outlook.com>
2024-08-14 20:56:48 +0300

    Create playGame() function to play 5 round Nest functions Add comments to clarify functions

--- script.js
+++ script.js
@@ -1,67 +1,85 @@
-// Computer
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getComputerChoice() {
-  let number = getRandomInt(1, 3);
-
-  let computerChoice = number == 1 ? "rock" : number == 2 ? "paper" : "scissors";
-  return computerChoice;
-}
-
-// Human
-
-function getHumanChoice() {
-  let humanChoice = prompt("Make your choice. \nRock \nPaper \nScissors");
-  return humanChoice;
-  // let choiceAlert = alert("Please choose amongst 3.");
-
-  // if (humanChoice == 1) {
-  //   humanChoice = "rock";
-  //   return humanChoice;
-  // } else if (humanChoice == 2) {
-  //   humanChoice = "paper";
-  //   return humanChoice;
-  // } else if (humanChoice == 3) {
-  //   humanChoice = "scissors";
-  //   return humanChoice;
-  // } else {
-  //   return choiceAlert;
-  // }
-}
-
 // Game Logic
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  
+  // Scoreboard
 
-function playRound () {
-  let computerChoice = getComputerChoice();
-  let humanChoice = getHumanChoice();
-  let noWinner = `Draw. \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
-  let winner;
+  let humanScore = 0;
+  let computerScore = 0;
 
-  if (humanChoice.toLowerCase() === computerChoice) {
-    return alert(noWinner);
+  // Functions to generate a random number between a range
+  // and get computer choice depending on that random number
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-    else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors" || 
-    humanChoice.toLowerCase() === "paper" && computerChoice === "rock" || 
-    humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+  
+  function getComputerChoice() {
+    let number = getRandomInt(1, 3);
+  
+    let computerChoice =
+      number == 1 ? "rock" : number == 2 ? "paper" : "scissors";
+    return computerChoice;
+  }
+
+  // Function to prompt user to get their choice
+
+  function getHumanChoice() {
+    let humanChoice = prompt("Make your choice. \nRock \nPaper \nScissors");
+    return humanChoice;
+  }
+
+  // Function to play a round
+
+  function playRound(humanChoice, computerChoice) {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+    let noWinner = `Draw. \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
+    let winner;
+
+    if (humanChoice.toLowerCase() === computerChoice) {
+      return alert(noWinner);
+    } else if (
+      (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
+      (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
+      (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper")
+    ) {
       humanScore++;
-      winner = "Human"
-    }
-    else if ((computerChoice === "rock" && humanChoice.toLowerCase() === "scissors") || 
-    (computerChoice === "paper" && humanChoice.toLowerCase() === "rock") || 
-    (computerChoice === "scissors" && humanChoice.toLowerCase() === "paper")) {
+      winner = "human";
+    } else if (
+      (computerChoice === "rock" && humanChoice.toLowerCase() === "scissors") ||
+      (computerChoice === "paper" && humanChoice.toLowerCase() === "rock") ||
+      (computerChoice === "scissors" && humanChoice.toLowerCase() === "paper")
+    ) {
       computerScore++;
-      winner = "Computer"
-    }
-    else {
+      winner = "computer";
+    } else {
       return alert(`Invalid choice.`);
     }
 
-  let winnerIs = `${winner} wins! \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
-  return alert(winnerIs);
+    let winnerIs = `The computer has chosen ${computerChoice}!\nThe ${winner} wins! \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
+    return alert(winnerIs);
+  }
+
+  // Play 5 round and return human wins IF
+  // human score is bigger than computer score ENDIF
+    // return computer wins ELSE IF computer score is bigger ENDELSEIF
+    // return tie ELSE
+
+  // I didn't use loops because I didn't learn it yet
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+  playRound();
+
+  if (humanScore < computerScore) {
+    alert(`The computer wins the game!`);
+  }
+    else if (humanScore > computerScore) {
+      alert(`The human wins the game!`);
+    }
+    else {
+      alert(`The game is a tie!`);
+    }
 }
