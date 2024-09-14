0096d94df9c56f1fa9e2c0d74335d52289e5dbe5
serverigit <serverigit@outlook.com>
2024-09-14 18:26:51 +0300

    Add UI to play game

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
-// Game Logic
+// Play 5 round and return human wins IF
+// human score is bigger than computer score ENDIF
+// return computer wins ELSE IF computer score is bigger ENDELSEIF
+// return tie ELSE
+//
+//   if (humanScore < computerScore) {
+//     alert(`The computer wins the game!`);
+//   } else if (humanScore > computerScore) {
+//     alert(`The human wins the game!`);
+//   } else {
+//     alert(`The game is a tie!`);
+//   }
 
-function playGame() {
-  
-  // Scoreboard
+// Buttons
+// add click event listeners to all playBtn buttons
+// 1. get name value of the clicked button
+// 2. call playRound function
 
-  let humanScore = 0;
-  let computerScore = 0;
+const playBtns = document.querySelectorAll(".play-btn");
+const winnerDiv = document.createElement("div");
+document.body.append(winnerDiv);
 
-  // Functions to generate a random number between a range
-  // and get computer choice depending on that random number
+let humanScore = 0;
+let computerScore = 0;
 
-  function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  
-  function getComputerChoice() {
-    let number = getRandomInt(1, 3);
-  
-    let computerChoice =
-      number == 1 ? "rock" : number == 2 ? "paper" : "scissors";
-    return computerChoice;
-  }
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-  // Function to prompt user to get their choice
+function getComputerChoice() {
+  let number = getRandomInt(1, 3);
+  let computerChoice =
+    number == 1 ? "rock" : number == 2 ? "paper" : "scissors";
+  return computerChoice;
+}
 
-  function getHumanChoice() {
-    let humanChoice = prompt("Make your choice. \nRock \nPaper \nScissors");
-    return humanChoice;
-  }
-
-  // Function to play a round
-
-  function playRound(humanChoice, computerChoice) {
-    computerChoice = getComputerChoice();
-    humanChoice = getHumanChoice();
+playBtns.forEach((playBtn) => {
+  playBtn.addEventListener("click", () => {
+    const humanChoice = playBtn.getAttribute("name");
+    const computerChoice = getComputerChoice();
     let noWinner = `Draw. \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
-    let winner;
+    let winner = "";
 
     if (humanChoice.toLowerCase() === computerChoice) {
-      return alert(noWinner);
+      winnerDiv.textContent = noWinner;
+      return;
     } else if (
       (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") ||
       (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") ||
@@ -57,29 +62,17 @@ function playGame() {
       return alert(`Invalid choice.`);
     }
 
+    if (computerScore === 5) {
+      winnerDiv.textContent = `The computer wins the game!`;
+      return;
+    } else if (humanScore === 5) {
+      winnerDiv.textContent = `The human wins the game!`;
+      humanScore = 0;
+      computerScore = 0;
+      return;
+    }
+
     let winnerIs = `The computer has chosen ${computerChoice}!\nThe ${winner} wins! \n\nHuman score: ${humanScore} \nComputer score: ${computerScore}`;
-    return alert(winnerIs);
-  }
-
-  // Play 5 round and return human wins IF
-  // human score is bigger than computer score ENDIF
-    // return computer wins ELSE IF computer score is bigger ENDELSEIF
-    // return tie ELSE
-
-  // I didn't use loops because I didn't learn it yet
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-  playRound();
-
-  if (humanScore < computerScore) {
-    alert(`The computer wins the game!`);
-  }
-    else if (humanScore > computerScore) {
-      alert(`The human wins the game!`);
-    }
-    else {
-      alert(`The game is a tie!`);
-    }
-}
+    winnerDiv.textContent = winnerIs;
+  });
+});
